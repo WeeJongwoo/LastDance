da722b5d5fafd81e2a4bd3186f0d5be042114e53
Wee <wjw0518@gmail.com>
2026-05-08 01:03:03 +0900

    Hazard Actor 사용할 AnimNotify 제작

---
+++ Source/LastDance/Animation/AnimNotify_SpawnHazard.cpp
@@ -0,0 +1,63 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AnimNotify_SpawnHazard.h"
+#include "Combat/LDHazardActor.h"
+#include "Data/LDHazardDataAsset.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "GameFramework/Pawn.h"
+#include "Engine/World.h"
+
+void UAnimNotify_SpawnHazard::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+    Super::Notify(MeshComp, Animation, EventReference);
+
+    if (!MeshComp)
+    {
+        return;
+    }
+
+    AActor* Owner = MeshComp->GetOwner();
+    if (!Owner)
+    {
+        return;
+    }
+
+    if (!Owner->HasAuthority())
+    {
+        return;
+    }
+
+    if (!HazardClass || !HazardData)
+    {
+        return;
+    }
+
+    UWorld* World = Owner->GetWorld();
+    if (!World)
+    {
+        return;
+    }
+
+    FVector SpawnLocation = Owner->GetActorLocation();
+    FRotator SpawnRotation = Owner->GetActorRotation();
+
+    if (!SpawnSocketName.IsNone() && MeshComp->DoesSocketExist(SpawnSocketName))
+    {
+        SpawnLocation = MeshComp->GetSocketLocation(SpawnSocketName);
+    }
+
+    SpawnLocation += Owner->GetActorRotation().RotateVector(SpawnOffset);
+
+    const FTransform SpawnTransform(SpawnRotation, SpawnLocation);
+
+    APawn* InstigatorPawn = Cast<APawn>(Owner);
+
+    ALDHazardActor* Spawned = World->SpawnActorDeferred<ALDHazardActor>(HazardClass, SpawnTransform, 
+        Owner, InstigatorPawn, ESpawnActorCollisionHandlingMethod::AlwaysSpawn);
+
+    if (!Spawned) return;
+
+    Spawned->InitializeFromData(HazardData);
+    Spawned->FinishSpawning(SpawnTransform);
+}
---
+++ Source/LastDance/Animation/AnimNotify_SpawnHazard.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "AnimNotify_SpawnHazard.generated.h"
+
+class ALDHazardActor;
+class ULDHazardDataAsset;
+
+UCLASS()
+class LASTDANCE_API UAnimNotify_SpawnHazard : public UAnimNotify
+{
+	GENERATED_BODY()
+	
+	
+public:
+
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+
+protected:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hazard")
+	TSubclassOf<ALDHazardActor> HazardClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hazard")
+	TObjectPtr<ULDHazardDataAsset> HazardData;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hazard")
+	FName SpawnSocketName;   
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hazard")
+	FVector SpawnOffset = FVector::ZeroVector;
+};
